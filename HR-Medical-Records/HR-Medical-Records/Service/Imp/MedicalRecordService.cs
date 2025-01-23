b61fb89086edc8316ae9df8af1652085d66d83d6
Alex <alexabrahamian.cba@gmail.com>
2025-01-23 15:04:28 -0300

    Fix implementation

--- HR-Medical-Records/HR-Medical-Records/Service/Imp/MedicalRecordService.cs
+++ HR-Medical-Records/HR-Medical-Records/Service/Imp/MedicalRecordService.cs
@@ -4,5 +4,9 @@ namespace HR_Medical_Records.Service.Imp
 {
     public class MedicalRecordService : IMedicalRecordService
     {
+        public object GetMedicalRecordById(int medicalRecordId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
